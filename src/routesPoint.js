2414fe154c0f6d20826cfe6fc445100cb572288a
Andre Costa <andrevgcosta02@gmail.com>
2024-09-30 00:32:44 -0300

    Deletando arquivos do controle de ponto

--- src/routesPoint.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const PointController = require("./controllers/PointController")
-
-router.get("/listAll/", PointController.listAll)
-router.post("/register", PointController.register)
-
-module.exports = router
--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const bodyParser = require("body-parser")
 const redis = require('redis');
 
 const routes = require("./routes")
-const routesPoint = require("./routesPoint")
 const server = express()
 const port = 8080;
 
@@ -14,9 +13,7 @@ server.use(bodyParser.json())
 server.use(cors())
 
 server.use("/employee", routes)
-server.use("/PontoEletronico", routesPoint)
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
